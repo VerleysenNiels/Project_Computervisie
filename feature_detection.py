885c4c48fef3926e94704f9bf84132ab270a45d6
Niels Verleysen <nlsverleysen99@gmail.com>
2019-04-02 15:30:16 +0200

    Find intersections from detected lines

--- feature_detection.py
+++ feature_detection.py
@@ -2,5 +2,34 @@ import cv2
 import numpy as np
 
 
-def detect_lines():
-    pass
+# Calculate intersection between two lines
+def parametricIntersect(r1, t1, r2, t2):
+    ct1 = np.cos(t1)     # matrix element a
+    st1 = np.sin(t1)     # b
+    ct2 = np.cos(t2)     # c
+    st2 = np.sin(t2)     # d
+    d = ct1*st2-st1*ct2        # determinative (rearranged matrix for inverse)
+    if d != 0.0:
+        x = int((st2*r1-st1*r2)/d)
+        y = int((-ct2*r1+ct1*r2)/d)
+        return((x, y))
+    else: # lines are parallel and will NEVER intersect!
+        return(None)
+
+# Detect lines in grayscale image
+def detect_lines(img):
+    lines = cv2.HoughLines(img, 1, np.pi / 180, 200)
+
+    intersections = []
+
+    for line1 in range(0, len(lines)-1):
+        for line2 in range(line1+1, len(lines)):
+            point = parametricIntersect(lines[line1][0], lines[line1][1], lines[line2][0], lines[line2][1])
+            if point is not None:
+                intersections.append([line1, line2, point[0], point[1]])
+
+
+
+
+
+

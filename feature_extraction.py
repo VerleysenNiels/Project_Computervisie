ceca670667f37f109d7d8be4b3d7d6e0c9c156c7
Robrecht <robrechtmeersman@hotmail.com>
2019-04-27 11:07:27 +0200

    Append color feature vector to edges vector

--- feature_extraction.py
+++ feature_extraction.py
@@ -23,11 +23,16 @@ class FeatureExtraction(object):
             filtered = cv2.filter2D(bw, cv2.CV_32F, _filter)
             filtered = self.max_pool(np.array(filtered))
             features.append(filtered)
-            viz_utils.imshow(filtered, norm=True, resize=True)
+            # viz_utils.imshow(filtered, norm=True, resize=True)
 
         features = np.array(features)
-
-        print(features.shape)
+        features = features.reshape((12, -1))
+        colors = self.extract_colors(image)
+        logging.debug('colors.shape = %s', colors.shape)
+        logging.debug('features.shape = %s', features.shape)
+        features = np.append(features, colors, axis=0)
+        logging.info('features.shape = %s', features.shape)
+        return features
 
     def create_dog(self, dim, angle=0):
         center = (dim // 2, dim // 2)
@@ -48,42 +53,44 @@ class FeatureExtraction(object):
         kernel = cv2.warpAffine(kernel, rot_mat, (dim, dim))
         return kernel
 
-    def max_pool(self, image, size=16):
+    def max_pool(self, image, block_size=16):
         """Assumes dimensions are n*size"""
         # Init array of shape (12, x/16, y/16)
-        res = np.zeros((image.shape[0] // size, image.shape[1] // size),
+        res = np.zeros((image.shape[0] // block_size, image.shape[1] // block_size),
                        dtype=np.float)
 
         for row in range(res.shape[0]):
             for col in range(res.shape[1]):
-                arr_slice = image[row * size:row *
-                                  size + size, col * size:col * size + size]
+                arr_slice = image[row * block_size:row *
+                                  block_size + block_size, col * block_size:col * block_size + block_size]
                 res[row, col] = np.max(np.abs(arr_slice))
         return res
 
-    def extract_colors(self, img, block_size):
-    """ Extract average color for each block with given size
-        Return a list for each color with the average values for that color
-        Input must be a BGR image and a valid size for the blocks
-    """
+    def extract_colors(self, img, block_size=16):
+        """ Extract average color for each block with given size
+            Return a list for each color with the average values for that colorf
+            Input must be a BGR image and a valid size for the blocks
+        """
+        red = []
+        green = []
+        blue = []
 
-    red = []
-    green = []
-    blue = []
+        for y in range(0, img.shape[0]//block_size):
+            for x in range(0, img.shape[1] // block_size):
+                blue.append(np.average(
+                    img[y*block_size:y*block_size+block_size, x*block_size:x*block_size+block_size, 0]))
+                green.append(np.average(
+                    img[y*block_size:y*block_size+block_size, x*block_size:x*block_size+block_size, 1]))
+                red.append(np.average(
+                    img[y*block_size:y*block_size+block_size, x*block_size:x*block_size+block_size, 2]))
 
-    for y in range(0, img.shape[0]//block_size):
-        for x in range(0, img.shape[1] // block_size):
-            blue.append(np.average(
-                img[y*block_size:y*block_size+block_size, x*block_size:x*block_size+block_size, 0]))
-            green.append(np.average(
-                img[y*block_size:y*block_size+block_size, x*block_size:x*block_size+block_size, 1]))
-            red.append(np.average(
-                img[y*block_size:y*block_size+block_size, x*block_size:x*block_size+block_size, 2]))
-
-    return [red, green, blue]
+        return np.array([red, green, blue])
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        format="[%(levelname)s] %(asctime)s - %(message)s", level=logging.DEBUG
+    )
     extr = FeatureExtraction()
     for path, img in io_utils.imread_folder('./db'):
         extr.extract_features(img)

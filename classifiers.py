26bd00922ce256cdc77e0beff92a5febb05c8738
Niels Verleysen <nlsverleysen99@gmail.com>
2019-04-27 14:46:00 +0200

    Neural network classifier works
    Accuracy is very low, probably due to a shortage of data
    
    TEST WITH: python main.py eval -v -v

--- classifiers.py
+++ classifiers.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
+from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn.model_selection import cross_val_score
 from keras.models import Sequential
 from keras.layers import Dense
 import keras.optimizers as optimizers
@@ -27,19 +29,39 @@ class RandomForestClassifier(object):
 class NeuralNetClassifier(object):
 
     def __init__(self):
-        self.learning_rate = 3e-4
-        self.model = Sequential()
-        self.model.add(Dense(1024, activation='elu', input_shape=(15, 4489)))
-        self.model.add(Dense(256, activation='elu'))
-        self.model.add(Dense(64, activation='elu'))
-        self.model.add(Dense(14, activation='softmax'))
-        self.model.compile(loss=losses.mean_squared_error, optimizer=optimizers.Adam(lr=self.learning_rate))
+        self.learning_rate = 1e-3
+        self.labels = ["zaal_a", "zaal_b", "zaal_c", "zaal_d", "zaal_e", "zaal_f", "zaal_g", "zaal_h", "zaal_i", "zaal_j", "zaal_k", "zaal_l", "zaal_m", "zaal_n"]
+        self.model = self.create_model()
 
     def train(self, X, y):
-        self.model.fit(X, y, verbose=True)
+        n = len(X)
+        idx = np.arange(n)
+        np.random.seed(456)
+        np.random.shuffle(idx)
+        x_shuffle = X[idx, ...]
+        y_shuffle = y[idx, ...]
+        self.model.fit(x_shuffle, self.transform_output(y_shuffle), verbose=True, batch_size=len(X)-1)
 
     def eval(self, X, y):
-        return self.model.evaluate(X, y)
+        return self.model.evaluate(X, self.transform_output(y))[1]
 
     def infer(self, X):
         return self.model.predict(X)
+
+    def transform_output(self, y):
+        result = []
+        for v in y:
+            a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            a[self.labels.index(v)] = 1
+            result.append(a)
+        return result
+
+    def create_model(self):
+        model = Sequential()
+        model.add(Dense(1024, activation='elu', input_shape=(15360,)))
+        model.add(Dense(256, activation='elu'))
+        model.add(Dense(256, activation='elu'))
+        model.add(Dense(64, activation='elu'))
+        model.add(Dense(14, activation='softmax'))
+        model.compile(loss=losses.mean_squared_error, optimizer=optimizers.sgd(lr=self.learning_rate), metrics=['accuracy'])
+        return model

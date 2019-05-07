4ed437d704d0b359d75f864247335f93bad17bed
Ralph Moens <ralph.moens@ugent.be>
2019-05-07 17:27:28 +0200

    update corner labeling
    Removed old code (trials) that were in comment - nothing actually changed.
    Rooms 7 and 8 done

--- label_corners.py
+++ label_corners.py
@@ -14,20 +14,11 @@ def draw_point (event, x, y, flags, corners):
         cv2.circle(img_scaled, (x,y), 3, (0,255,0), thickness=-1)
         cv2.imshow(imname, img_scaled)
 
-"""
-for path, img in io_utils.imread_folder("images\zalen"):
-    path2 = path.split("\\")
-    imname = path2[len(path2)-1]
-    cv2.namedWindow(imname)
-    corners = []
-    cv2.imshow(imname, img)
-    cv2.setMouseCallback('image', draw_point, corners, img)
-"""
 
-with open("corners_6.csv", mode = "w") as pandc:
+with open("corners_8.csv", mode = "w") as pandc:
     corner_writer = csv.writer(pandc, delimiter=";", quotechar="\"", quoting=csv.QUOTE_MINIMAL)
 
-    for path, img in io_utils.imread_folder("images\zalen\zaal_6"):
+    for path, img in io_utils.imread_folder("images\zalen\zaal_8"):
         path2 = path.split("\\")
         imname = path2[len(path2) - 1]
         print(imname)
@@ -52,16 +43,3 @@ with open("corners_6.csv", mode = "w") as pandc:
             corner_writer.writerow([imname, corners[0], corners[1], corners[2], corners[3],
                                 corners[4], corners[5], corners[6], corners[7]])
 
-    """
-    img = cv2.imread("images\zalen\zaal_1\IMG_20190323_111717.jpg")
-    imname = "IMG_20190323_111717.jpg"
-    height, width, depth = img.shape
-    img_scaled = cv2.resize(img, (int(width/scale), int(height/scale))) #width height
-    cv2.namedWindow(imname)
-    corners = []
-    cv2.setMouseCallback(imname, draw_point, corners)
-    cv2.imshow(imname, img_scaled)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    corner_writer.writerow([imname, corners[0], corners[1], corners[2], corners[3],
-                            corners[4], corners[5], corners[6], corners[7]])"""
